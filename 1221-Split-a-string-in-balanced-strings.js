f4f0c8d8c2e8892e7519a3948137416f6fc5be82
truongtrongtin <truongtrongtin0305@gmail.com>
2020-03-14 22:56:44 +0700

    Split a String in Balanced Strings

---
+++ 1221-Split-a-string-in-balanced-strings.js
@@ -0,0 +1,47 @@
+/*
+Balanced strings are those who have equal quantity of 'L' and 'R' characters.
+Given a balanced string s split it in the maximum amount of balanced strings.
+Return the maximum amount of splitted balanced strings.
+
+Example 1:
+Input: s = "RLRRLLRLRL"
+Output: 4
+Explanation: s can be split into "RL", "RRLL", "RL", "RL", each substring contains same number of 'L' and 'R'.
+
+Example 2:
+Input: s = "RLLLLRRRLR"
+Output: 3
+Explanation: s can be split into "RL", "LLLRRR", "LR", each substring contains same number of 'L' and 'R'.
+
+Example 3:
+Input: s = "LLLLRRRR"
+Output: 1
+Explanation: s can be split into "LLLLRRRR".
+
+Example 4:
+Input: s = "RLRRRLLRLL"
+Output: 2
+Explanation: s can be split into "RL", "RRRLLRLL", since each substring contains an equal number of 'L' and 'R'
+
+Constraints:
+1 <= s.length <= 1000
+s[i] = 'L' or 'R'
+*/
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+// TC: O(n) SC: O(1)
+const balancedStringSplit = function(s) {
+  let balance = 0
+  let res = 0
+  for (let i = 0; i < s.length; i++) {
+    balance += s[i] === "R" ? 1 : -1
+    if (balance === 0) res++
+  }
+  return res
+};
+
+console.log(balancedStringSplit("RLRRLLRLRL"))
